82e56892d2ced760e3ba7542936ac6ee319c05de
fatbardheminii <fatbardh.emini@hotmail.com>
2023-11-25 13:23:58 +0100

    Add playRound function and conditionals

--- script.js
+++ script.js
@@ -10,4 +10,28 @@ function getComputerChoice(){
     console.log(`Computer choice is: ${computerChoice}`);
 }
 
-getComputerChoice();
+function playRound(computerSelection, playerSelection){
+    if(computerSelection === playerSelection) {
+        return 'Result of the game: DRAW!';
+    } else if(computerSelection === 'rock' && playerSelection === 'paper'){
+        return 'Paper beats rock \n The winner is: Player!';
+    } else if('rock' && playerSelection === 'scissors'){
+        return 'Rock beats scissors \n The winner is: Computer!';
+    } else if('paper' && playerSelection === 'rock'){
+        return 'Paper beats rock \n The winner is: Computer!';
+    } else if('paper' && playerSelection === 'scissors'){
+        return 'Scissors beat paper \n The winner is Player!';
+    } else if('scissors' && playerSelection === 'rock'){
+        return 'Rock beats scissors \n The winner is: Player!';
+    } else if('scissors' && playerSelection === 'paper'){
+        return 'Paper beats scissors \n The winner is: Computer!';
+    } else {
+        return 'Use only allowed choices: ROCK, PAPER OR SCISSORS!'
+    }
+}
+
+let computerChoice = getComputerChoice();
+const playerChoice = prompt("Make your choice:").toLowerCase();
+console.log(`Player choice is: ${playerChoice}`);
+
+console.log(playRound(computerChoice, playerChoice));
