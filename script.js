6fda438cb6a2f17401ec21100306bf887550002f
fatbardheminii <fatbardh.emini@hotmail.com>
2023-11-27 00:13:02 +0100

    Add for loop, global variables
    add for loop to play 5 rounds, check scores, end result

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-function getComputerChoice(){
-    let computerChoice = Math.floor(Math.random() * 3);
-    if(computerChoice === 0){
-        computerChoice = 'rock';
-    } else if(computerChoice === 1){
-        computerChoice = 'paper';
-    } else if(computerChoice === 2){
-        computerChoice = 'scissors';
-    }
-    console.log(`Computer choice is: ${computerChoice}`);
-    return computerChoice;
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  if (computerChoice === 0) {
+    computerChoice = "rock";
+  } else if (computerChoice === 1) {
+    computerChoice = "paper";
+  } else if (computerChoice === 2) {
+    computerChoice = "scissors";
+  }
+  console.log(`Computer choice is: ${computerChoice}`);
+  return computerChoice;
 }
 
-function playRound(computerSelection, playerSelection){
-    if(computerSelection === playerSelection) {
-        return 'Result of the game: DRAW!';
-    } else if(computerSelection === 'rock' && playerSelection === 'paper'){
-        return 'Paper beats rock \n The winner is: Player!';
-    } else if (computerSelection === 'computerSelection === rock' && playerSelection === 'scissors') {
-      return 'Rock beats scissors \n The winner is: Computer!';
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-      return 'Paper beats rock \n The winner is: Computer!';
-    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-      return 'Scissors beat paper \n The winner is Player!';
-    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-      return 'Rock beats scissors \n The winner is: Player!';
-    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-      return 'Scissors beat paper \n The winner is: Computer!';
-    } else {
-      return 'Use only allowed choices: ROCK, PAPER OR SCISSORS!';
-    }
+function playRound(computerSelection, playerSelection) {
+  if (computerSelection === playerSelection) {
+    return "Result of the round: DRAW!";
+  } else if (computerSelection === "rock" && playerSelection === "paper") {
+    playerScore++;
+    return "Paper beats rock \n The winner of the round is: Player!";
+  } else if (
+    computerSelection === "computerSelection === rock" &&
+    playerSelection === "scissors"
+  ) {
+    computerScore++;
+    return "Rock beats scissors \n The winner of the round is: Computer!";
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    computerScore++;
+    return "Paper beats rock \n The winner of the round is: Computer!";
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    playerScore++;
+    return "Scissors beat paper \n The winner of the round is Player!";
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    playerScore++;
+    return "Rock beats scissors \n The winner of the round is: Player!";
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    computerScore++;
+    return "Scissors beat paper \n The winner of the round is: Computer!";
+  } else {
+    return "Use only allowed choices: ROCK, PAPER OR SCISSORS!";
+  }
 }
 
-let computerChoice = getComputerChoice();
-const playerChoice = prompt('Make your choice:').toLowerCase();
-console.log(`Player choice is: ${playerChoice}`);
+let playerScore = 0;
+let computerScore = 0;
 
-console.log(playRound(computerChoice, playerChoice));
+for (let round = 1; round < 6; round++) {
+  console.log(`Round: ${round}`);
+  let computerChoice = getComputerChoice();
+  const playerChoice = prompt("Make your choice:").toLowerCase();
+  console.log(`Player choice is: ${playerChoice}`);
+  console.log(playRound(computerChoice, playerChoice));
+  console.log(
+    `Player score is: ${playerScore} \n Computer score is: ${computerScore}`
+  );
+}
+
+if (playerScore === computerScore) {
+  console.log("The game after 5 rounds ends in DRAW");
+} else if (playerScore > computerScore) {
+  console.log("The Winner is Player");
+} else {
+  console.log("The Winner is Computer");
+}
